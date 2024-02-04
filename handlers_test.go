79faef9bc1897644189b4139728eda166f38294e
dlandolfi <16345531+dlandolfi@users.noreply.github.com>
2024-02-03 23:01:00 -0500

    starting handlers test

---
+++ handlers_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHomeHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(homeHandler)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body
+	expected := "Hello World"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestTestPublic(t *testing.T) {
+	// Create a request with a dummy path
+	req, err := http.NewRequest("GET", "/testpublic", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+
+	// Create a handler function
+	handler := http.HandlerFunc(testPublic)
+
+	// Serve the HTTP request to the ResponseRecorder
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body
+	expected := "Public route successful"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
