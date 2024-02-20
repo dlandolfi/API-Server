26ae691599763b7f791243f8e472c8313096d653
dlandolfi <16345531+dlandolfi@users.noreply.github.com>
2024-02-19 19:01:08 -0500

    cleanup

--- backend/routes.go
+++ backend/routes.go
@@ -9,15 +9,11 @@ import (
 
 func setupRoutes(r *mux.Router) {
 	// Defining routes
-	// r.HandleFunc("/", homeHandler)
-
-	// buildHandler := http.FileServer(http.Dir("../build"))
-	// r.PathPrefix("/_app").Handler(buildHandler)
 
 	r.HandleFunc("/api/v1/testpublic", testPublic).Methods(http.MethodGet, http.MethodOptions)
 	r.Handle("/api/v1/testprivate", authMiddleware(http.HandlerFunc(testPrivate)))
 	r.Handle("/api/v1/testauthenticated", authMiddleware(http.HandlerFunc(testAuthenticated)))
-	r.HandleFunc("/api/v1/getuser", getUserHandler).Methods(http.MethodGet, http.MethodOptions) // /getuser?id=n
+	r.HandleFunc("/api/v1/getuser", getUserHandler).Methods(http.MethodGet, http.MethodOptions)
 	r.HandleFunc("/api/v1/insertuser", createUserInDb).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/api/v1/getallusers", getAllUsersHandler).Methods(http.MethodGet, http.MethodOptions)
 
