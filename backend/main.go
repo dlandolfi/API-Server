eb10616ac4948fbeaba96f25f62554dae95efd36
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2024-06-06 19:23:08 -0400

    adding redis to docker-compose

--- backend/main.go
+++ backend/main.go
@@ -76,7 +76,7 @@ func ExampleClient() {
 	var ctx = context.Background()
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
