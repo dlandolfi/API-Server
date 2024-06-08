12d0777c6b519172caac4407b3a23ace40b5820f
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2024-06-07 22:47:47 -0400

    fetch working, needs cleanup

--- fetcher/main.go
+++ fetcher/main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/redis/go-redis/v9"
 )
 
+func fetchPrice() ([]byte, error) {
+	resp, err := http.Get("https://api.metals.dev/v1/metal/spot?api_key=1XXURGAUJCZZAWTFJPHB808TFJPHB&metal=gold&currency=USD")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch gold price: status %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	return body, nil
+}
+
 func main() {
-	ExampleClient()
+	// ExampleClient()
+	response, err := fetchPrice()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(response))
 }
 
 func ExampleClient() {
