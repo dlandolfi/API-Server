c1ee2ad1cc9506a2d7defa36a60b7d3ad672be6b
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2024-06-21 23:37:39 -0400

    adding coffee rss

--- ruby_server/app/controllers/api_controller.rb
+++ ruby_server/app/controllers/api_controller.rb
@@ -1,5 +1,21 @@
+require 'rss'
+require 'open-uri'
+
 class ApiController < ApplicationController
   def newsfeed
-    render json:{message:"newsfeed"}
+    rss_url = 'https://www.coffeereview.com/feed/' # Replace with the actual RSS feed URL
+    rss_content = URI.open(rss_url).read
+    rss = RSS::Parser.parse(rss_content, false)
+
+    transformed_items = rss.items.map do |item|
+      {
+        title: item.title,
+        link: item.link,
+        pub_date: item.pubDate
+      }
+    end
+
+    render json: transformed_items
   end
 end
+
